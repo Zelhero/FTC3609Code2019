d9ff2678e932b2f5786bc46abdba4f86f84001e7
zelhero <zeldaheroemail@yahoo.com>
2018-09-11 00:41:58 -0500

    Added hardware files to work on

---
+++ ftc_app-master/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Hardware/MovementFunc.java
@@ -0,0 +1,12 @@
+
+
+public class MovementFunc
+
+
+        FluffyHardwarePushbot robot       = new FluffyHardwarePushbot();
+
+    public void Drive(float Left, float Right){
+
+        robot.leftDrive.setPower()
+
+        }
